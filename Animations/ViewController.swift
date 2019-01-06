8c0534fca3bf33a9aa86d4b7293a01837a35a533
SHS <SHS@macbook-pro-de-shs.local>
2019-01-06 20:53:12 +0100

    Initial Commit

---
+++ Animations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animations
+//
+//  Created by SHS on 6/1/19.
+//  Copyright © 2019 SHS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
